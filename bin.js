e3c1855c7c24396e284b71ed40311fafa15820dc
Mathias Buus <mathiasbuus@gmail.com>
2015-12-28 02:00:04 +0100

    keep announcing/looking up in cli

--- bin.js
+++ bin.js
@@ -7,20 +7,20 @@ var argv = minimist(process.argv.slice(2), {
   alias: {port: 'p', host: 'h', tracker: 't'}
 })
 
+var rcvd = {}
 var cmd = argv._[0]
-var disc
+var disc = discovery(argv)
 
 if (cmd === 'listen') {
-  disc = discovery(argv)
   disc.listen(argv.port, onlisten)
 } else if (cmd === 'lookup') {
-  disc = discovery(argv)
-  disc.on('peer', console.log)
-  disc.lookup(argv._[1])
+  disc.on('peer', onpeer)
+  lookup()
+  setInterval(lookup, 1000)
 } else if (cmd === 'announce') {
-  disc = discovery(argv)
   if (!argv.port) throw new Error('You need to specify --port')
-  disc.announce(argv._[1], argv)
+  announce()
+  setInterval(announce, 1000)
 } else {
   console.error(
     'dns-discovery [command]\n' +
@@ -36,6 +36,20 @@ if (cmd === 'listen') {
     '     --ttl=(optional ttl in seconds)\n'
   )
   process.exit(1)
+}
+
+function lookup () {
+  disc.lookup(argv._[1])
+}
+
+function announce () {
+  disc.announce(argv._[1], argv)
+}
+
+function onpeer (name, peer) {
+  if (rcvd[peer.id]) return
+  rcvd[peer.id] = true
+  console.log(name, peer)
 }
 
 function onlisten (err) {
